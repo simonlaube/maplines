98ba0d3fd0a97dab5a7011a04a51ba8d5c6f7a4f
simonlaube <simon.laube@stud.unibas.ch>
2022-07-27 14:04:42 +0200

    first major code refactoring completed

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,17 +9,15 @@ extern crate gpx;
 extern crate geojson;
 extern crate time;
 
-mod io;
 mod import;
-mod gps_analyzer;
 mod track_analysis;
 mod type_converter;
 mod paths;
 
+use std::fs;
 use std::path::PathBuf;
 use geojson::GeoJson;
-use io::{MaplinesDirPaths, LinePaths};
-use gps_analyzer::{GpsSummary, read_gpx};
+use track_analysis::TrackAnalysis;
 use type_converter::write_gpx_to_geojson;
 use gpx::Gpx;
 use tauri::api::dialog;
@@ -66,7 +64,7 @@ fn main() {
       }
       _ => {}
     })
-    .invoke_handler(tauri::generate_handler![load_gps_summaries, load_line, load_geojson])
+    .invoke_handler(tauri::generate_handler![load_geojson, load_track_analysis])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
   paths::create_dirs_if_not_exist();
@@ -74,65 +72,35 @@ fn main() {
 }
 
 #[tauri::command]
-fn load_gps_summaries() -> Vec<GpsSummary> {
-  let maplines_paths = MaplinesDirPaths::get();
-  let gps_paths: Vec<PathBuf> = std::fs::read_dir(maplines_paths.main.clone()).unwrap()
+fn load_track_analysis() -> Vec<TrackAnalysis> {
+  let paths: Vec<PathBuf> = std::fs::read_dir(paths::track_analysis()).unwrap()
     .into_iter()
-    .map(|x| x.unwrap().path()).collect();
-  let summary_paths: Vec<PathBuf> = std::fs::read_dir(maplines_paths.summary.clone()).unwrap()
-    .into_iter()
-    .map(|x| x.unwrap().path()).collect();
-    
-  let mut gps_summaries: Vec<GpsSummary> = vec![];
-
-  for g in gps_paths {
-    
-    // Clone of path with json extension used for comparison.
-    let mut g_json = g.clone();
-    g_json.set_extension("json");
-
-    // Gps file already analyzed.
-    if summary_paths.iter().any(|a| a.as_path().file_name().unwrap().eq(g_json.as_path().file_name().unwrap())) {
-      let mut json_path_in = maplines_paths.summary.clone();
-      json_path_in.push(g_json.as_path().file_name().unwrap());
-      gps_summaries.push(GpsSummary::from_json(json_path_in));
-      continue;
+    .map(|x| x.unwrap().path())
+    .filter(|x| x.file_name().unwrap().to_str().unwrap().ends_with(".json"))
+    .collect();
+  let mut result: Vec<TrackAnalysis> = vec![];
+  for p in paths {
+    match TrackAnalysis::from_json_path(&p) {
+      Ok(ta) => result.push(ta),
+      _ => println!("Could not load track with path {:?}.", p),
     }
-
-    // Analyze Gpx file, write summary and geojson file.
-    let g_clone = g.clone();
-    if g_clone.as_os_str().to_str().unwrap().ends_with(".gpx") {
-      let mut geo_path = g_json;
-      geo_path.set_extension("geojson");
-      let file_name = String::from(geo_path.file_name().unwrap().to_str().unwrap());
-
-      let mut geo_path = maplines_paths.geojson.clone();
-      geo_path.push(file_name.clone());
-      write_gpx_to_geojson(read_gpx(&g_clone), file_name, geo_path);
-
-      let agf = GpsSummary::from_gpx(g);
-      agf.write(maplines_paths.summary.clone());
-      gps_summaries.push(agf);
-      continue;
-    }
-
-    // TODO: Add support for further file types
   }
-
-  gps_summaries
+  result
 }
 
 #[tauri::command]
-fn load_line(file_name: String) -> Gpx {
-  let maplines_paths = MaplinesDirPaths::get();
-  let mut file_path = maplines_paths.main;
-  file_path.push(file_name);
-  let gpx = gps_analyzer::read_gpx(&file_path);
-  gpx
-}
+fn load_geojson(ulid: String) -> Option<GeoJson> {
+  let mut line_path = paths::geojson();
+  line_path.push(ulid);
+  line_path.set_extension("geojson");
 
-#[tauri::command]
-fn load_geojson(file_name: String) -> GeoJson {
-  let mut line_paths = LinePaths::new_from_name(file_name);
-  line_paths.read_geojson().parse::<GeoJson>().unwrap()
+  match fs::read_to_string(line_path) {
+    Ok(s) => {
+      match s.parse::<GeoJson>() {
+        Ok(g) => Some(g),
+        _ => None,
+      }
+    },
+    _ => None,
+  }
 }
--- src-tauri/src/track_analysis.rs
+++ src-tauri/src/track_analysis.rs
@@ -1,5 +1,7 @@
-use ulid::Ulid;
+use gpx::Track;
 use serde::{Serialize, Deserialize};
+use std::path::PathBuf;
+use std::fs;
 /// same as Track but without links and segments
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TrackAnalysis {
@@ -17,4 +19,12 @@ pub struct TrackAnalysis {
     pub x_max: (f64, f64),
     pub y_min: (f64, f64),
     pub y_max: (f64, f64),
+}
+
+impl TrackAnalysis {
+    pub fn from_json_path(json_path_in: &PathBuf) -> Result<TrackAnalysis, std::io::Error> {
+        let json_string = fs::read_to_string(json_path_in)?;
+        let ta: TrackAnalysis = serde_json::from_str(&json_string.as_str())?;
+        Ok(ta)
+    }
 }
