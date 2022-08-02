ad3435df142adf7bb8a33ea56f6d7962cbef4eb2
simonlaube <simon.laube@stud.unibas.ch>
2022-08-02 18:39:41 +0200

    optimized pause detection

--- src-tauri/src/pause_finder.rs
+++ src-tauri/src/pause_finder.rs
@@ -16,7 +16,9 @@ const MIN_ABS_DIST_SEC: f64 = 0.5;
 /// relative dist / abs dist
 const MIN_RATIO: f64 = 0.4;
 /// Clusters within this range get declared as clusters
-const SCATTER_RADIUS: f64 = 50.0;
+const SCATTER_RADIUS: f64 = 30.0;
+/// Intervals greater than MAX_INTERVAL are considered pauses or teleports
+const MAX_INTERVAL: i64 = 8;
 
 /// Consecutive points must lie within a SCATTER_RADIUS for at least MIN_CLUSTER_TIME to be considered a cluster
 const MIN_CLUSTER_TIME: i64 = 40;
@@ -70,59 +72,42 @@ fn find_time_jumps(gpx: &Gpx, min_break_time: u64) -> Result<Option<Vec<Pause>>,
 
 /// Find consecutive gps points building 'clusters' within a constrained area
 fn find_clusters(gpx: &Gpx, geojson: &GeoJson) -> Vec<Pause> {
-    /*
-    for (i, p) in gpx.tracks[0].segments[0].points.iter().enumerate() {
-        let mut cluster_start: geo::Point = p.point().into();
-        for (j, q) in gpx.tracks[0].segments[0].points[i+1..].iter().enumerate() {
-            // Find first point outside SCATTER_RADIUS
-            if cluster_start.haversine_distance(&q.point().into()) > SCATTER_RADIUS {
-                let diff = OffsetDateTime::from(q.time.unwrap()).unix_timestamp() - OffsetDateTime::from(p.time.unwrap()).unix_timestamp();
-                // Check if time reaching this point exceeded MIN_CLUSTER_TIME
-                if diff > MIN_CLUSTER_TIME {
-                    println!("{}", diff);
-                    break; 
-                }
-            }
-            //println!("{}", coord_1.euclidean_distance(&Coordinate::from(q.point())));
-            // coord_1.haversine_distance(&q.point().into());
-            // println!("{}", coord_1.haversine_distance(&q.point().into()));
-            /*if coord_1.euclidean_distance(&Coordinate::from(q.point())) < 5 {
-
-            }*/
-        }
-    }
-    */
-
     let mut result: Vec<Pause> = vec![];
     let mut detection_completed = false;
     let mut pos = 0;
     let mut time_in_radius: i64 = 0;
-    let mut last_center: Point<f64> = Point::new(0.0, 0.0);
+    let mut center: Point<f64> = Point::new(0.0, 0.0);
+    let mut cluster: Vec<&Waypoint> = vec![];
     while !detection_completed {
 
         detection_completed = true;
         let start_point = gpx.tracks[0].segments[0].points[pos].point();
         let start_time = OffsetDateTime::from(gpx.tracks[0].segments[0].points[pos].time.unwrap()).unix_timestamp();
-        let mut cluster: Vec<&Waypoint> = vec![];
+        let mut current_cluster: Vec<&Waypoint> = vec![];
         for q in gpx.tracks[0].segments[0].points[pos..].iter() {
-            if start_point.haversine_distance(&q.point().into()) < SCATTER_RADIUS || cluster.len() < 2 {
-                cluster.push(q);
+            if start_point.haversine_distance(&q.point().into()) < SCATTER_RADIUS || current_cluster.len() < 2 {
+                current_cluster.push(q);
             } else {
                 let current_time_in_radius = OffsetDateTime::from(q.time.unwrap()).unix_timestamp() - start_time;
-                let (index, center) = point_closest_to_center(&cluster);
+                let (index, current_center) = point_closest_to_center(&current_cluster);
                 // check if more time was spent in the current radius than in the last
                 if current_time_in_radius > time_in_radius {
                     time_in_radius = current_time_in_radius;
-                    last_center = center;
+                    center = current_center;
+                    cluster = current_cluster;
                     pos += index; // This loses time spent until index (CHECK THIS!!!)
                     detection_completed = false;
                     break;
                 // check if last cluster was pause or not and continue
                 } else {
                     if time_in_radius > MIN_CLUSTER_TIME {
-                        result.push(Pause { point_before: last_center.into(), point_after: last_center.into(), duration_sec: current_time_in_radius as u64 })
+                        let c = improve_cluster(&cluster, &center);
+                        // let c = &cluster;
+                        result.push(Pause { point_before: c.first().unwrap().point().into(), point_after: c.last().unwrap().point().into(), duration_sec: time_in_radius as u64 });
+                        result.push(Pause { point_before: c.last().unwrap().point().into(), point_after: c.last().unwrap().point().into(), duration_sec: time_in_radius as u64 });
+                        result.push(Pause { point_before: center.into(), point_after: center.into(), duration_sec: time_in_radius as u64 });
                     }
-                    pos += cluster.len();
+                    pos += current_cluster.len();
                     time_in_radius = 0;
                     detection_completed = false;
                     break;
@@ -151,8 +136,45 @@ fn point_closest_to_center(cluster: &Vec<&Waypoint>) -> (usize, Point<f64>) {
     if index == 0 {
         index = 1;
     }
-    println!("{}", index);
     (index, center)
+}
+
+fn improve_cluster<'a>(cluster: &Vec<&'a Waypoint>, center: &'a Point<f64>) -> Vec<&'a Waypoint> {
+    let mut start_center_dist: f64 = cluster[0].point().haversine_distance(center).abs();
+    let mut start_index = 0;
+    let init_cluster_len = cluster.len();
+    let mut c = cluster.clone();
+    let mut last_time = OffsetDateTime::from(cluster[0].time.unwrap()).unix_timestamp();
+    for (i, p) in cluster[1..].iter().enumerate() {
+        if p.point().haversine_distance(center) < start_center_dist &&
+        (OffsetDateTime::from(p.time.unwrap()).unix_timestamp() - last_time).abs() < MAX_INTERVAL {
+            last_time = OffsetDateTime::from(p.time.unwrap()).unix_timestamp();
+            start_center_dist = p.point().haversine_distance(center);
+            start_index = i + 1;
+        } else {
+            break;
+        }
+    }
+    c = c.split_at(start_index).1.into();
+    c.reverse();
+    last_time = OffsetDateTime::from(c[0].time.unwrap()).unix_timestamp();
+    let mut end_center_dist: f64 = c[0].point().haversine_distance(center).abs();
+    let mut end_index = 0;
+    for (i, p) in c[1..].iter().enumerate() {
+        if p.point().haversine_distance(center).abs() < end_center_dist &&
+        (last_time - OffsetDateTime::from(p.time.unwrap()).unix_timestamp()).abs() < MAX_INTERVAL {
+            last_time = OffsetDateTime::from(p.time.unwrap()).unix_timestamp();
+            end_center_dist = p.point().haversine_distance(center);
+            end_index = i + 1;
+        } else {
+            break;
+        }
+    }
+    
+    c = c.split_at(end_index).1.into();
+    c.reverse();
+    c
+
 }
 
 #[derive(Serialize, Deserialize, Debug)]
